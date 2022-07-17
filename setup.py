d5ffb7617bc095a7f23ea57a6e426cb214ea7302
JosephMichaelSale <107808046+JosephMichaelSale@users.noreply.github.com>
2022-07-17 13:46:19 -0400

    Updated setup.py
    Changed setup import source from distutils.core to setuptools
    Changed version from 1.0 to 1.0.0
    Added following new meta-data fields to setup:  author, author_email, url, python_requires, install_requires

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='datamapy',
-    version='1.0',
-    py_modules=['datamapy.map','datamapy.access','datamapy.reorder'],
+	name='datamapy',
+	version='1.0.0',
+	author='Joseph Sale',
+	author_email='Joseph.Michael.Sale@gmail.com',
+	url='https://github.com/JosephMichaelSale/datamapy',
+	py_modules=['datamapy.map','datamapy.access','datamapy.reorder'],
+	python_requires='>3.8',
+	install_requires=['Pillow >= 8.4.0'],
 )
